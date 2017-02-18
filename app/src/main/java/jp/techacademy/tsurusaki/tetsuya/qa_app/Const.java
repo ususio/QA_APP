4761ed2b9f614983a5530f52ac8b32c8b74df072
鶴崎哲也 <ususio04@tsurusakitetsuya-no-MacBook-Air.local>
2017-02-18 13:20:56 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/tsurusaki/tetsuya/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.tsurusaki.tetsuya.qa_app;
+
+/**
+ * Created by ususio04 on 2017/01/31.
+ */
+
+    public class Const {
+        public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+        public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+        public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+        public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
